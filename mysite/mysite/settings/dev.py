3be6f0d3bc1c1c17c4dc41d5083343dd10119b9f
dizzyplay <vovoboss@gmail.com>
2019-03-19 14:50:11 +0900

    init

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,20 @@
+from .common import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+    },
+}
+
+INSTALLED_APPS += [
+    'corsheaders'
+]
+
+CORS_ORIGIN_ALLOW_ALL=True

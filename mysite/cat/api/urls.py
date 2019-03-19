71dfb8ce01c17c1923991f0e39d253064f07a966
dizzyplay <vovoboss@gmail.com>
2019-03-19 15:11:52 +0900

    cat app setting

---
+++ mysite/cat/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'cat'
+
+urlpatterns = [
+    path('cat/', views.main)
+]
---
+++ mysite/cat/api/views.py
@@ -0,0 +1,4 @@
+from django.http.response import HttpResponse
+
+def main(request):
+    return HttpResponse("hello")

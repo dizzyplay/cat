c809983296f8b1a728315628ef045e00e7b8e62b
dizzyplay <vovoboss@gmail.com>
2019-03-23 14:40:34 +0900

    gunicorn nginx

---
+++ mysite/settings/prod.py
@@ -0,0 +1,20 @@
+from .common import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['django']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME','cat'),
+        'USER': os.environ.get('DB_USER','postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD','postgres'),
+        'HOST': os.environ.get('DB_HOST','localhost')
+    },
+}
+
+INSTALLED_APPS += [
+    'corsheaders'
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
